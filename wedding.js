594467969874650b9f000e17c41cf653606c9c53
BradenOlive <oliv6000@mylaurier.ca>
2022-07-14 20:33:02 -0400

    Create wedding.js

---
+++ wedding.js
@@ -0,0 +1,24 @@
+class Wedding {
+
+    constructor(name, type, theme, selectedVendors, finalizedVendors, year, month, day) {
+        this.name = name;
+        this.type = type;
+        this.theme = theme;
+        this.selectedVendors = selectedVendors;
+        this.finalizedVendors = finalizedVendors;
+        this.date = new Date(year,month,day);
+
+    }
+
+    addVendor() {
+
+    }
+
+    deleteVendor() {
+
+    }
+
+    finalizeVendor() {
+        
+    }
+}
