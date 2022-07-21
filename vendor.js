11c0b06abb776139e3a9c8879d9176eb437a59bc
BradenOlive <oliv6000@mylaurier.ca>
2022-07-21 18:40:47 -0400

    Created Sub-Classes of Vendor

--- vendor.js
+++ vendor.js
@@ -3,15 +3,14 @@
 class Vendor extends User {
 
 
-    constructor(userName, password, personId, age, Location, vendorName, vendorId, phoneNumber, businessType, Location) {
+    constructor(userName, password, personId, age, Location, vendorName, vendorId, phoneNumber, businessType) {
     
         super(userName, password, personId, age); //call to constructor of parent class: User
 
-        this.vendorName = vendorName;
-        this.vendorId = vendorId;
-        this.phoneNumber = phoneNumber;
+        this.vendorName   = vendorName;
+        this.vendorId     = vendorId;
+        this.phoneNumber  = phoneNumber;
         this.businessType = businessType;
-        this.Location = Location;
 
     }
 
