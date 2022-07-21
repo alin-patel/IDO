81e33b5bd4497c0f8ada7a33577141a807c2fd12
BradenOlive <oliv6000@mylaurier.ca>
2022-07-21 18:51:28 -0400

    Added export statements

--- ProductProvidor.js
+++ ProductProvidor.js
@@ -1,9 +1,9 @@
 
-class ProductProvidor extends Vendor {
+export default class ProductProvidor extends Vendor {
 
     constructor(username, password, personId, age, Location, vendorName, vendorId, phoneNumber, businessType, productName, productPrice, productSize, productDeliveryTime) {
         
-        super(username, password, personId, age, Location, vendorName, vendorId, phoneNumber, businessType);
+        super(username, password, personId, age, Location, vendorName, vendorId, phoneNumber, businessType); //call to constructor of parent class: Vendor
 
         this.productName         = productName;
         this.productPrice        = productPrice;
