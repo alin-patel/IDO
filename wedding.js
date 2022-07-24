9d2af2c836e98b8293a3f932554a7f7a0a860622
RituA08 <81048032+RituA08@users.noreply.github.com>
2022-07-24 13:51:00 -0400

    Update wedding.js

--- wedding.js
+++ wedding.js
@@ -1,37 +1,51 @@
-import ServiceProvider from 'ServiceProvider.js';
-import ProductProvidor from 'ProductProvidor.js';
+
 
 class Wedding {
 
-    constructor(name, type, theme, selectedVendors, finalizedVendors, year, month, day) {
-        this.name = name;
-        this.type = type;
-        this.theme = theme;
-        this.selectedVendors = selectedVendors;
-        this.finalizedVendors = finalizedVendors;
-        this.date = new Date(year,month,day);
+    #selectedVendors = [];
+    #finalizedVendors = [];
+    #name = null;
+    #type = null;
+    #theme = null;
+    #date = null;
 
+    constructor(name, type, theme, year, month, day) {
+        this.#name = name;
+        this.#type = type;
+        this.#theme = theme;
+        this.#date = new Date(year,month,day);
     }
 
-    addVendor() {
-
+    addVendor(Vendor v) {
+        if (this.#selectedVendors.includes(v.vendorName) == false){
+            this.#selectedVendors.push(v);
+        }
     }
 
-    deleteVendor() {
-
+    deleteVendor(Vendor v) {
+        for (var i = 0; i < selected_vendors.length; i++){
+            if (this.#selected_vendors[i].vendorName == v.vendorName){
+                this.#selected_vendors.splice(i, 1);
+                break;
+            }
+        }
+        return;
+    
     }
 
-    finalizeVendor() {
-        
-    }
-
-    fetchWeddingInfo() { //export a class info to JSON
-        let objectParse = JSON.stringify(this);
-        
-        const fs = require('fs');
-        const path = require('path');
-        fs.writeFileSync(path.resolve(__dirname, 'vendor_' + this.name + '.json'),objectParse);
+    finalizeVendor(Vendor v) {
+        if (this.#finalizedVendors.includes(v.vendorName) == false){
+            this.#finalizedVendors.push(v);
+        }
     }
     
-}
+    fetchWeddingInfo() { //export a class info to JSON
+	        let objectParse = JSON.stringify(this);
+	        
+	        const fs = require('fs');
+	        const path = require('path');
+	        fs.writeFileSync(path.resolve(__dirname, 'vendor_' + this.name + '.json'),objectParse);
+    }
+
+} 
 
